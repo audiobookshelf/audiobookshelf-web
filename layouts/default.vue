18af2590e6e0a4e6115dec4215402bb5d4425f02
Mark Cooper <mcoop320@gmail.com>
2021-09-15 16:02:37 -0500

    init

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-screen h-screen">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  mounted() {}
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="page-wrapper" class="w-full h-full bg-black">
+    <div class="h-full flex flex-col items-center justify-center">
+      <img src="/Logo.png" class="h-40" />
+      <h1 class="text-white text-5xl pt-8 font-book">AudioBookshelf</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
