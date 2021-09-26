462d2b3f8d5ff6abd009c9a25b5102fb42c23814
advplyr <advplyr@protonmail.com>
2021-09-26 17:19:18 -0500

    Docs page

--- pages/docs.vue
+++ pages/docs.vue
@@ -1,6 +1,162 @@
 <template>
   <div class="w-full h-full max-w-5xl mx-auto px-2">
-    <p>Not done yet..</p>
+    <div class="flex -ml-8">
+      <div>
+        <span class="material-icons text-warning text-2xl">priority_high</span>
+      </div>
+      <p class="pl-2 text-lg">AudioBookshelf relies on you already having an organized directory structure. This is because information is taken from your folder names. This includes the author name, series name, book title, publish year, volume number, and subtitle.</p>
+    </div>
+
+    <div class="w-full bg-white bg-opacity-20 h-px my-8" />
+
+    <h1 id="structure" class="text-3xl mb-4">
+      <nuxt-link to="#structure"><span class="material-icons text-xl text-gray-400 hover:text-white cursor-pointer mr-2">tag</span></nuxt-link
+      >Directory Structure
+    </h1>
+
+    <p class="mb-4">Here is an example supported directory structure</p>
+
+    <div class="p-4 border border-opacity-10 bg-primary bg-opacity-20 font-mono">
+      <div class="flex py-1 max-w-2xl">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Terry Goodkind</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Author</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;</span>
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Sword of Truth</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Series</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Vol 1 - 1994 - Wizards First Rule</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Title&nbsp;</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <span class="material-icons">audiotrack</span>
+        <p class="pl-2">Audio Track 1.mp3</p>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <span class="material-icons">audiotrack</span>
+        <p class="pl-2">Audio Track 2.mp3</p>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <span class="material-icons">crop_original</span>
+        <p class="pl-2">Cover.jpg</p>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Vol 2 - 1995 - Stone of Tears</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Title&nbsp;</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <span class="material-icons">audiotrack</span>
+        <p class="pl-2">Audiobook.m4b</p>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;</span>
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Heart of Black Ice - Sister of Darkness</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Title&nbsp;</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <span class="material-icons">audiotrack</span>
+        <p class="pl-2">Audio File.m4a</p>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Steven Levy</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Author</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;</span>
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Hackers - Heroes of the Computer Revolution</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Title&nbsp;</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;&nbsp;-&nbsp;</span>
+        <span class="material-icons">audiotrack</span>
+        <p class="pl-2">Audio File.m4a</p>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">1945 - Animal Farm</p>
+        <div class="flex-grow" />
+        <span class="text-white text-opacity-50 pl-4">&lt;-- Title&nbsp;</span>
+      </div>
+      <div class="flex py-1 max-w-2xl">
+        <span class="text-white text-opacity-25">&nbsp;-&nbsp;</span>
+        <span class="material-icons">audiotrack</span>
+        <p class="pl-2">Audiobook.mp3</p>
+      </div>
+    </div>
+
+    <p class="my-4">Files must always be in a directory with the book title. <span class="text-error">Files in the root directory are ignored.</span><br />The title directory can be in the root directory, an author directory, or a series directory.</p>
+
+    <div class="w-full bg-white bg-opacity-20 h-px my-8" />
+
+    <h1 id="naming" class="text-3xl mb-4">
+      <nuxt-link to="#naming"><span class="material-icons text-xl text-gray-400 hover:text-white cursor-pointer mr-2">tag</span></nuxt-link
+      >Naming Convention
+    </h1>
+
+    <p class="mb-4">In addition to the book title, the title folder can include the publish year, volume number, and the subtitle.</p>
+    <p class="mb-4">Here are a bunch of ways the same book could be named:</p>
+
+    <div class="p-4 border border-opacity-10 bg-primary bg-opacity-20 font-mono">
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Wizards First Rule</p>
+      </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">1994 - Wizards First Rule</p>
+      </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Wizards First Rule - A Really Good Subtitle</p>
+      </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">1994 - Book 1 - Wizards First Rule</p>
+      </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Vol 1 - 1994 - Wizards First Rule</p>
+      </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">1994 - Wizards First Rule - Volume 1</p>
+      </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">Vol. 1 - 1994 - Wizards First Rule - A Really Good Subtitle</p>
+      </div>
+    </div>
+
+    <ul class="list-inside list-disc my-4">
+      <li class="list-item">Parsing out subtitles into a separate field is optional and must be enabled in settings.</li>
+      <li>Different parts of a name should be separated by " - ", with the exception of the volume number (spaces aren't needed).</li>
+      <li>The publish year must be the first part of the name OR directly after a volume number, and separated by " - " on both sides.</li>
+    </ul>
+
+    <div class="w-full h-px my-24" />
   </div>
 </template>
 
