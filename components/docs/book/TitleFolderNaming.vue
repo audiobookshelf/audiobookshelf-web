7675ff2eff4d1de21d8b61cc681853f03587fb8f
Cassie Esposito <dev@timevault.org>
2022-05-24 18:27:39 -0700

    Updated documentation to reflect changes to volume number naming rules introduced during refactor

--- components/docs/book/TitleFolderNaming.vue
+++ components/docs/book/TitleFolderNaming.vue
@@ -31,6 +31,10 @@
       </div>
       <div class="flex py-1">
         <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">1994 - Volume 1. Wizards First Rule {Sam Tsoutsouvas}</p>
+      </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
         <p class="pl-2">Vol 1 - 1994 - Wizards First Rule</p>
       </div>
       <div class="flex py-1">
@@ -49,13 +53,21 @@
         <img src="/folder.svg" class="h-6" />
         <p class="pl-2">1 - Wizards First Rule</p>
       </div>
+      <div class="flex py-1">
+        <img src="/folder.svg" class="h-6" />
+        <p class="pl-2">1. Wizards First Rule</p>
+      </div>
     </div>
 
     <ul class="list-outside md:list-inside px-6 md:px-0 list-disc my-4 text-sm md:text-base">
-      <li class="py-1"><strong class="underline">Subtitle: </strong>Parsing out subtitles into a separate field is optional and must be enabled in settings. Subtitle must be separated by " - ".</li>
-      <li class="py-1"><strong class="underline">Volume Number: </strong>The volume number can be the first number in the folder name separated by " - " (see last example) OR anywhere in the folder name if "Vol", "Book", or "Volume" is before the number. Case insensitive & decimals supported.</li>
-      <li class="py-1"><strong class="underline">Publish Year: </strong>The publish year must be the first part of the name OR directly after a volume number, and separated by " - " on both sides.</li>
-      <li class="py-1"><strong class="underline">Narrator: </strong>The Narrator must be at the end of the folder name in {curly braces}.</li>
+      <li class="py-1"><strong><u>Subtitle</u>:</strong> Parsing out subtitles into a separate field is optional and must be enabled in settings. Subtitle must be separated by " - ".</li>
+      <li class="py-1"><strong><u>Volume Number</u>:</strong> Case insensitive & decimals supported.</li>
+        <ul>
+          <li style="padding-left: 3em">The volume number can be placed anywhere in the folder name.</li>
+          <li style="padding-left: 3em">It must be followed by " - " or ". "</li>
+          <li style="padding-left: 3em">If it is not at the beginning of the folder name, it must be preceded by " - " and "Vol" "Vol." "Volume" or "Book"</li>
+        </ul>
+      <li class="py-1"><strong><u>Publish Year</u>:</strong> The publish year must be the first part of the name OR directly after a volume number, and separated by " - " on both sides.</li>
     </ul>
   </div>
 </template>
