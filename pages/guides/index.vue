f896fe4a3252d60a23d2c18753194fc6b5d66e4d
advplyr <advplyr@protonmail.com>
2022-08-08 18:22:57 -0500

    Guides index page

---
+++ pages/guides/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-screen p-8 max-w-5xl mx-auto">
+    <h1 class="text-3xl">Guides</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  mounted() {}
+}
+</script>
