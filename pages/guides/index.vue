a1f2df106240d9db12c31b8dd0f0ab70f61c6b46
advplyr <advplyr@protonmail.com>
2022-08-08 18:27:06 -0500

    Add link to docker-install

--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="w-full h-screen p-8 max-w-5xl mx-auto">
-    <h1 class="text-3xl">Guides</h1>
+    <h1 class="text-3xl mb-4">Guides</h1>
+
+    <nuxt-link to="/guides/docker-install">Install ABS using docker</nuxt-link>
   </div>
 </template>
 
