023e71177a2806b5f483e60215f86b463f6d9c64
Nicholas <nicholaslwallace@gmail.com>
2024-01-06 20:27:22 -0700

    Revert "Removed old vue files"
    This reverts commit edd1448ae172f53ffee15a57968c0508a46f72c0.

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full h-full bg-gradient overflow-hidden">
+    <div class="w-full bg-primary h-64">
+      <div class="flex h-full justify-center items-center">
+        <p class="text-4xl">Privacy Policy</p>
+      </div>
+    </div>
+    <div class="py-16 text-center max-w-xl mx-auto">
+      <p class="text-base mb-4 underline">We don't store your data, period.</p>
+      <p>Audiobookshelf is free and open source, so you can always have a look for yourself. We <em>encourage</em> that.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  mounted() {}
+}
+</script>
