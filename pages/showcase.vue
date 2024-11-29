eae77b2723e647b2352988aa09ed206556521d8d
advplyr <advplyr@protonmail.com>
2024-11-29 16:39:17 -0600

    Update showcase to include web client demo link & new layout for screenshots

--- pages/showcase.vue
+++ pages/showcase.vue
@@ -2,74 +2,27 @@
   <div class="w-full max-w-5xl mx-auto px-2 py-8 min-h-full">
     <div class="flex -ml-8 mb-6">
       <div>
-        <span class="material-icons text-warning text-2xl">priority_high</span>
+        <span class="material-icons text-warning text-xl">priority_high</span>
       </div>
-      <p class="pl-2 text-base md:text-lg">Consider sending screenshots of your audiobookshelf to <a href="mailto:advplyr@protonmail.com" class="text-blue-500 underline">advplyr@protonmail.com</a> (or <a href="https://github.com/audiobookshelf/audiobookshelf-web" target="_blank" class="text-blue-500 underline">submit a PR</a>) so we can show it off!</p>
+      <div>
+        <p class="pl-2 text-base mb-2">Check out the web client demo: <a href="https://audiobooks.dev/" target="_blank" class="text-blue-500 underline">https://audiobooks.dev/</a> (thanks for hosting <a href="https://github.com/Vito0912" target="_blank" class="text-blue-500 underline">@Vito0912</a>!)</p>
+        <p class="pl-2">Username/password: <code>demo/demo</code> (user account)</p>
+      </div>
     </div>
 
-    <div class="py-8">
-      <img src="https://github.com/advplyr/audiobookshelf/raw/master/images/DemoLibrary.png" />
+    <div class="flex flex-wrap">
+      <div v-for="image in images" :key="image.title" class="w-full md:w-1/2 p-2">
+        <div>
+          <p class="text-sm mb-2 text-gray-200">{{ image.title }}</p>
+          <img :src="image.src" class="cursor-pointer" @click="showImage(image)" />
+        </div>
+      </div>
     </div>
 
-    <div class="py-8">
-      <div class="flex items-center justify-between">
-        <p class="text-lg mb-2">Series view</p>
-
-        <p class="text-sm mt-2 text-gray-200">from&nbsp;<a href="https://noted.lol/audiobookshelf-a-self-hosted-audiobook-and-podcast-server-with-phone-apps/" target="_blank" class="underline text-blue-500 hover:text-blue-300">noted.lol</a></p>
+    <div v-if="showImageModal" class="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center" @click="showImageModal = false">
+      <div class="w-full h-full">
+        <img :src="selectedImage.src" class="w-full h-full object-contain" />
       </div>
-      <img src="/showcase/series.png" />
-    </div>
-
-    <div class="py-8">
-      <div class="flex items-center justify-between">
-        <p class="text-lg mb-2">Podcast library</p>
-
-        <p class="text-sm mt-2 text-gray-200">from&nbsp;<a href="https://noted.lol/audiobookshelf-a-self-hosted-audiobook-and-podcast-server-with-phone-apps/" target="_blank" class="underline text-blue-500 hover:text-blue-300">noted.lol</a></p>
-      </div>
-      <img src="/showcase/podcasts.png" />
-    </div>
-
-    <div class="py-8">
-      <div class="flex items-center justify-between">
-        <p class="text-lg mb-2">Matching book metadata</p>
-
-        <p class="text-sm mt-2 text-gray-200">from&nbsp;<a href="https://noted.lol/audiobookshelf-a-self-hosted-audiobook-and-podcast-server-with-phone-apps/" target="_blank" class="underline text-blue-500 hover:text-blue-300">noted.lol</a></p>
-      </div>
-      <img src="/showcase/match-book.gif" />
-    </div>
-
-    <div class="py-8">
-      <div class="flex items-center">
-        <p class="text-lg mb-2">Library stats</p>
-      </div>
-      <img src="/showcase/library-stats.png" />
-    </div>
-
-    <div class="py-8">
-      <div class="flex items-center justify-between">
-        <p class="text-lg mb-2">User stats</p>
-
-        <p class="text-sm mt-2 text-gray-200">from Colin</p>
-      </div>
-      <img src="/showcase/user-stats.png" />
-    </div>
-
-    <div class="py-8">
-      <div class="flex items-center justify-between">
-        <p class="text-lg mb-2">Authors view</p>
-
-        <p class="text-sm mt-2 text-gray-200">from Colin</p>
-      </div>
-      <img src="/showcase/authors-page.png" />
-    </div>
-
-    <div class="py-8">
-      <div class="flex items-center justify-between">
-        <p class="text-lg mb-2">Collections view</p>
-
-        <p class="text-sm mt-2 text-gray-200">from Colin</p>
-      </div>
-      <img src="/showcase/collections-page.png" />
     </div>
   </div>
 </template>
@@ -78,10 +31,60 @@
 export default {
   layout: 'guides',
   data() {
-    return {}
+    return {
+      showImageModal: false,
+      selectedImage: null,
+      images: [
+        {
+          src: '/showcase/home-books.png',
+          title: 'Home page'
+        },
+        {
+          src: '/showcase/library-books.png',
+          title: 'Library view'
+        },
+        {
+          src: 'https://github.com/advplyr/audiobookshelf/raw/master/images/DemoLibrary.png',
+          title: 'Library & player'
+        },
+        {
+          src: '/showcase/series.png',
+          title: 'Series view'
+        },
+        {
+          src: '/showcase/podcasts.png',
+          title: 'Podcast library'
+        },
+        {
+          src: '/showcase/match-book.gif',
+          title: 'Matching book metadata'
+        },
+        {
+          src: '/showcase/library-stats.png',
+          title: 'Library stats'
+        },
+        {
+          src: '/showcase/user-stats.png',
+          title: 'User stats'
+        },
+        {
+          src: '/showcase/authors-page.png',
+          title: 'Authors view'
+        },
+        {
+          src: '/showcase/collections-page.png',
+          title: 'Collections view'
+        }
+      ]
+    }
   },
   computed: {},
-  methods: {},
+  methods: {
+    showImage(image) {
+      this.selectedImage = image
+      this.showImageModal = true
+    }
+  },
   mounted() {}
 }
 </script>
